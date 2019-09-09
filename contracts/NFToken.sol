853d8eea3c1dc3495436ff4fc6e8fb0f4574f73c
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2019-09-10 02:02:51 +0800

    uint48 -> uint64, more cleanup

--- contracts/NFToken.sol
+++ contracts/NFToken.sol
@@ -10,37 +10,31 @@ pragma solidity >=0.4.24 <0.5.0;
  */
 contract NFToken {
 
-    uint256 constant SENDER = 0;
-    uint256 constant RECEIVER = 1;
-
+    /** cannot fractionalize non-fungibles */
     uint8 public constant decimals = 0;
     string public name;
     string public symbol;
     uint256 public totalSupply;
 
-    uint48 upperBound;
-    uint48[281474976710656] tokens;
-    mapping (uint48 => Range) rangeMap;
+    uint64 upperBound;
+    uint64[18446744073709551616] tokens;
+    mapping (uint64 => Range) rangeMap;
     mapping (address => Balance) balances;
     mapping (address => mapping (address => uint256)) allowed;
 
     struct Balance {
-        uint48 balance;
-        uint48[] ranges;
+        uint64 balance;
+        uint64[] ranges;
     }
 
     struct Range {
         address owner;
-        uint48 stop;
+        uint64 stop;
     }
 
 
-    event Transfer(address indexed from, address indexed to, uint256 tokens);
-    event Approval(
-        address indexed tokenOwner,
-        address indexed spender,
-        uint256 tokens
-    );
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event Approval(address indexed owner, address indexed spender, uint256 amount);
     event TransferRange(
         address indexed from,
         address indexed to,
@@ -97,8 +91,8 @@ contract NFToken {
         view
         returns (
             address _owner,
-            uint48 _start,
-            uint48 _stop
+            uint64 _start,
+            uint64 _stop
         )
     {
         _checkBounds(_idx);
@@ -112,7 +106,7 @@ contract NFToken {
         @param _owner Address to query
         @return Array of [(start, stop),..]
      */
-    function rangesOf(address _owner) external view returns (uint48[2][]) {
+    function rangesOf(address _owner) external view returns (uint64[2][]) {
         Balance storage b = balances[_owner];
         uint256 _count;
         for (uint256 i; i < b.ranges.length; i++) {
@@ -120,7 +114,7 @@ contract NFToken {
                 _count++;
             }
         }
-        uint48[2][] memory _ranges = new uint48[2][](_count);
+        uint64[2][] memory _ranges = new uint64[2][](_count);
         _count = 0;
         for (i = 0; i < b.ranges.length; i++) {
             if (b.ranges[i] > 0) {
@@ -139,26 +133,26 @@ contract NFToken {
      */
     function mint(
         address _owner,
-        uint48 _value
+        uint64 _value
     )
         external
         returns (bool)
     {
         require(_value > 0); // dev: mint 0
         require(upperBound + _value > upperBound); // dev: overflow
-        require(upperBound + _value <= 2**48 - 2); // dev: upper bound
-        uint48 _start = uint48(upperBound + 1);
-        uint48 _stop = _start + _value;
+        require(upperBound + _value <= 2**64 - 2); // dev: upper bound
+        uint64 _start = uint64(upperBound + 1);
+        uint64 _stop = _start + _value;
         if (rangeMap[tokens[upperBound]].owner == _owner) {
             /* merge with previous range */
-            uint48 _pointer = tokens[upperBound];
+            uint64 _pointer = tokens[upperBound];
             rangeMap[_pointer].stop = _stop;
         } else {
             /* create new range */
             _setRange(_start, _owner, _stop);
             balances[_owner].ranges.push(_start);
         }
-        uint48 _old = balances[_owner].balance;
+        uint64 _old = balances[_owner].balance;
         balances[_owner].balance += _value;
         totalSupply += _value;
         upperBound += _value;
@@ -174,9 +168,9 @@ contract NFToken {
         @param _stop Stop index of range to burn
         @return Bool success
      */
-    function burn(uint48 _start, uint48 _stop) external returns (bool) {
+    function burn(uint64 _start, uint64 _stop) external returns (bool) {
         require(_stop > _start); // dev: burn 0
-        uint48 _pointer = _getPointer(_stop-1);
+        uint64 _pointer = _getPointer(_stop-1);
         require(_pointer <= _start); // dev: multiple ranges
         address _owner = rangeMap[_pointer].owner;
         require(_owner != 0x00); // dev: already burnt
@@ -187,14 +181,33 @@ contract NFToken {
             _splitRange(_start);
         }
         _replaceInBalanceRange(_owner, _start, 0);
-        uint48 _value = _stop - _start;
+        uint64 _value = _stop - _start;
         totalSupply -= _value;
-        uint48 _old = balances[_owner].balance;
+        uint64 _old = balances[_owner].balance;
         balances[_owner].balance -= _value;
         emit Transfer(_owner, 0x00, _value);
         emit TransferRange(_owner, 0x00, _start, _stop, _value);
         rangeMap[_start].owner = 0x00;
         return true;
+    }
+
+    /**
+        @notice Splits a range
+        @dev
+            Called when a new tag is added, to prevent a balance range
+            where some tokens are tagged differently from others
+        @param _split Index to split the range at
+     */
+    function _splitRange(uint64 _split) internal {
+        if (tokens[_split-1] != 0 && tokens[_split] > tokens[_split-1]) return;
+        uint64 _pointer = _getPointer(_split);
+        Range storage r = rangeMap[_pointer];
+        uint64 _stop = r.stop;
+        r.stop = _split;
+        _replaceInBalanceRange(r.owner, 0, _split);
+        _setRangePointers(_pointer, _stop, 0);
+        _setRangePointers(_pointer, _split, _pointer);
+        _setRange(_split, r.owner, _stop);
     }
 
     /**
@@ -205,7 +218,7 @@ contract NFToken {
         @return bool success
      */
     function transfer(address _to, uint256 _value) external returns (bool) {
-        _transfer([msg.sender, _to], _value);
+        _transfer(msg.sender, _to, _value);
         return true;
     }
 
@@ -213,7 +226,7 @@ contract NFToken {
         @notice ERC-20 transferFrom standard
         @dev This will transfer tokens starting from balance.ranges[0]
         @param _from Sender address
-        @param _to Receipient address
+        @param _to Recipient address
         @param _value Number of tokens to send
         @return bool success
      */
@@ -225,12 +238,9 @@ contract NFToken {
         external
         returns (bool)
     {
-        require(
-            allowed[_from][msg.sender] >= _value,
-            "Insufficient allowance"
-        );
+        require(allowed[_from][msg.sender] >= _value, "Insufficient allowance");
         allowed[_from][msg.sender] -= _value;
-        _transfer([_from, _to], _value);
+        _transfer(_from, _to, _value);
         return true;
     }
 
@@ -242,25 +252,26 @@ contract NFToken {
         @param _value Amount to transfer
      */
     function _transfer(
-        address[2] _addr,
+        address _from,
+        address _to,
         uint256 _value
     )
         internal
     {
 
-        uint48 _smallVal = uint48(_value);
-        uint48[] memory _range;
+        uint64 _smallVal = uint64(_value);
+        uint64[] memory _range;
 
-        require(balances[_addr[SENDER]].balance >= _smallVal);
-        balances[_addr[SENDER]].balance -= _smallVal;
-        balances[_addr[RECEIVER]].balance += _smallVal;
+        require(balances[_from].balance >= _smallVal);
+        balances[_from].balance -= _smallVal;
+        balances[_to].balance += _smallVal;
 
-        emit Transfer(_addr[SENDER], _addr[RECEIVER], _value);
+        emit Transfer(_from, _to, _value);
         for (uint256 i; i < _range.length; i++) {
             if (_range[i] == 0) continue;
-            uint48 _start = _range[i];
-            uint48 _stop = rangeMap[_start].stop;
-            uint48 _amount = _stop - _start;
+            uint64 _start = _range[i];
+            uint64 _stop = rangeMap[_start].stop;
+            uint64 _amount = _stop - _start;
             if (_smallVal < _amount) {
                 _stop -= _amount - _smallVal;
                 _smallVal = 0;
@@ -270,8 +281,8 @@ contract NFToken {
             }
             _transferSingleRange(
                 _start,
-                _addr[SENDER],
-                _addr[RECEIVER],
+                _from,
+                _to,
                 _start,
                 _stop
             );
@@ -285,15 +296,15 @@ contract NFToken {
     /**
         @notice transfer tokens with a specific index range
         @dev Can send tokens into a custodian, but not out of one
-        @param _to Receipient address
+        @param _to Recipient address
         @param _start Transfer start index
         @param _stop Transfer stop index
         @return bool success
      */
     function transferRange(
         address _to,
-        uint48 _start,
-        uint48 _stop
+        uint64 _start,
+        uint64 _stop
     )
         external
         returns (bool)
@@ -301,30 +312,27 @@ contract NFToken {
         _checkBounds(_start);
         _checkBounds(_stop-1);
         require(_start < _stop); // dev: stop < start
-        uint48 _pointer = _getPointer(_stop-1);
+        uint64 _pointer = _getPointer(_stop-1);
         require(_pointer <= _start); // dev: multiple ranges
 
-        address[2] memory _addr = [msg.sender, _to];
-        uint48[2] memory _range = [_start, _stop];
-
-        uint48 _value = _stop - _start;
+        uint64 _value = _stop - _start;
 
         require(
-            _addr[SENDER] == rangeMap[_pointer].owner,
+            msg.sender == rangeMap[_pointer].owner,
             "Sender does not own range"
         );
-        require(_addr[SENDER] != _addr[RECEIVER], "Cannot send to self");
+        require(msg.sender != _to, "Cannot send to self");
 
-        require(_value <= balances[_addr[SENDER]].balance);
-        balances[_addr[SENDER]].balance -= _value;
-        balances[_addr[RECEIVER]].balance += _value;
+        require(_value <= balances[msg.sender].balance);
+        balances[msg.sender].balance -= _value;
+        balances[_to].balance += _value;
 
         _transferSingleRange(
             _pointer,
-            _addr[SENDER],
-            _addr[RECEIVER],
-            _range[0],
-            _range[1]
+            msg.sender,
+            _to,
+            _start,
+            _stop
         );
     }
 
@@ -337,17 +345,17 @@ contract NFToken {
         @param _stop Stop index of range
      */
     function _transferSingleRange(
-        uint48 _pointer,
+        uint64 _pointer,
         address _from,
         address _to,
-        uint48 _start,
-        uint48 _stop
+        uint64 _start,
+        uint64 _stop
     )
         internal
     {
         Range storage r = rangeMap[_pointer];
-        uint48 _rangeStop = r.stop;
-        uint48 _prev = tokens[_start-1];
+        uint64 _rangeStop = r.stop;
+        uint64 _prev = tokens[_start-1];
         emit TransferRange(_from, _to, _start, _stop, _stop-_start);
 
         if (_pointer == _start) {
@@ -415,7 +423,7 @@ contract NFToken {
             if (rangeMap[_stop].owner == _to) {
                 _replaceInBalanceRange(_to, _stop, _start);
                 _setRangePointers(_stop, rangeMap[_stop].stop, 0);
-                uint48 _next = rangeMap[_stop].stop;
+                uint64 _next = rangeMap[_stop].stop;
                 delete rangeMap[_stop];
                 _stop = _next;
             } else {
@@ -433,59 +441,33 @@ contract NFToken {
     }
 
     /**
-        @notice Splits a range
-        @dev
-            Called when a new tag is added, to prevent a balance range
-            where some tokens are tagged differently from others
-        @param _split Index to split the range at
-     */
-    function _splitRange(uint48 _split) internal {
-        if (tokens[_split-1] != 0 && tokens[_split] > tokens[_split-1]) return;
-        uint48 _pointer = _getPointer(_split);
-        Range storage r = rangeMap[_pointer];
-        uint48 _stop = r.stop;
-        r.stop = _split;
-        _replaceInBalanceRange(r.owner, 0, _split);
-        _setRangePointers(_pointer, _stop, 0);
-        _setRangePointers(_pointer, _split, _pointer);
-        _setRange(_split, r.owner, _stop);
-    }
-
-    /**
         @notice sets a Range struct and associated pointers
         @dev keeping this as a seperate method reduces gas costs from SSTORE
         @param _pointer Range pointer to set
         @param _owner Address of range owner
         @param _stop Range stop index
      */
-    function _setRange(
-        uint48 _pointer,
-        address _owner,
-        uint48 _stop
-    )
-        internal
-    {
+    function _setRange(uint64 _pointer, address _owner, uint64 _stop) internal {
         Range storage r = rangeMap[_pointer];
         if (r.owner != _owner) r.owner = _owner;
         if (r.stop != _stop) r.stop = _stop;
-
         _setRangePointers(_pointer, _stop, _pointer);
     }
 
     /**
         @notice internal - replace value in balance range array
-        @param _addr Balance addresss
+        @param _addr Balance address
         @param _old Token index to remove
         @param _new Token index to add
      */
     function _replaceInBalanceRange(
         address _addr,
-        uint48 _old,
-        uint48 _new
+        uint64 _old,
+        uint64 _new
     )
         internal
     {
-        uint48[] storage r = balances[_addr].ranges;
+        uint64[] storage r = balances[_addr].ranges;
         for (uint256 i; i < r.length; i++) {
             if (r[i] == _old) {
                 r[i] = _new;
@@ -503,7 +485,7 @@ contract NFToken {
         @param _stop Stop index of range
         @param _value Pointer value
      */
-    function _setRangePointers(uint48 _start, uint48 _stop, uint48 _value) internal {
+    function _setRangePointers(uint64 _start, uint64 _stop, uint64 _value) internal {
         tokens[_start] = _value;
         _stop -= 1;
         if (_start == _stop) return;
@@ -525,7 +507,7 @@ contract NFToken {
             and return the mapping pointer that the index is present within.
         @param i Token index
      */
-    function _getPointer(uint256 i) internal view returns (uint48) {
+    function _getPointer(uint256 i) internal view returns (uint64) {
         uint256 _increment = 1;
         while (true) {
             if (tokens[i] != 0x00) return tokens[i];
