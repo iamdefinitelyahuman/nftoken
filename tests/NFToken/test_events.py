9078dc8acec9f7db61acc5ec86afd616fe11bd0d
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2020-02-07 19:02:55 +0400

    add assertions, code cleanup

--- tests/NFToken/test_events.py
+++ tests/NFToken/test_events.py
@@ -4,9 +4,11 @@
 def test_transfer(accounts, nft):
     """transfer events"""
     tx = nft.transfer(accounts[2], 1000, {"from": accounts[1]})
+
     assert "Transfer" in tx.events
     expected = {"from": accounts[1], "to": accounts[2], "amount": 1000}
     assert tx.events["Transfer"] == expected
+
     assert "TransferRange" in tx.events
     expected = {"from": accounts[1], "to": accounts[2], "start": 1, "stop": 1001, "amount": 1000}
     assert tx.events["TransferRange"] == expected
@@ -16,9 +18,11 @@ def test_transferFrom(accounts, nft):
     """transferFrom events"""
     nft.approve(accounts[3], 1000, {"from": accounts[1]})
     tx = nft.transferFrom(accounts[1], accounts[2], 1000, {"from": accounts[3]})
+
     assert "Transfer" in tx.events
     expected = {"from": accounts[1], "to": accounts[2], "amount": 1000}
     assert tx.events["Transfer"] == expected
+
     assert "TransferRange" in tx.events
     expected = {"from": accounts[1], "to": accounts[2], "start": 1, "stop": 1001, "amount": 1000}
     assert tx.events["TransferRange"] == expected
@@ -27,9 +31,11 @@ def test_transferFrom(accounts, nft):
 def test_transferRange(accounts, nft):
     """transferRange events"""
     tx = nft.transferRange(accounts[2], 1, 1001, {"from": accounts[1]})
+
     assert "Transfer" in tx.events
     expected = {"from": accounts[1], "to": accounts[2], "amount": 1000}
     assert tx.events["Transfer"] == expected
+
     assert "TransferRange" in tx.events
     expected = {"from": accounts[1], "to": accounts[2], "start": 1, "stop": 1001, "amount": 1000}
     assert tx.events["TransferRange"] == expected
--- tests/NFToken/test_no_transfer.py
+++ tests/NFToken/test_no_transfer.py
@@ -1,15 +1,42 @@
 #!/usr/bin/python3
 
 
-def test_same_addr(accounts, nft):
-    """send to self"""
+def test_same_addr_transferRange(accounts, nft):
+    ranges = nft.rangesOf(accounts[2])
     nft.transferRange(accounts[2], 11000, 12000, {"from": accounts[2]})
-    nft.transfer(accounts[1], 1000, {"from": accounts[1]})
-    nft.approve(accounts[1], 1000, {"from": accounts[1]})
-    nft.transferFrom(accounts[1], accounts[1], 1000, {"from": accounts[1]})
+
+    assert nft.rangesOf(accounts[2]) == ranges
+    assert nft.balanceOf(accounts[2]) == 10000
 
 
-def test_zero_tokens(accounts, nft):
-    """send zero tokens"""
-    nft.transfer(accounts[1], 0, {"from": accounts[1]})
+def test_same_addr_transfer(accounts, nft):
+    ranges = nft.rangesOf(accounts[2])
+    nft.transfer(accounts[2], 1000, {"from": accounts[2]})
+
+    assert nft.rangesOf(accounts[2]) == ranges
+    assert nft.balanceOf(accounts[2]) == 10000
+
+
+def test_same_addr_transferFrom(accounts, nft):
+    ranges = nft.rangesOf(accounts[2])
+    nft.approve(accounts[1], 1000, {"from": accounts[2]})
+    nft.transferFrom(accounts[2], accounts[2], 1000, {"from": accounts[1]})
+
+    assert nft.rangesOf(accounts[2]) == ranges
+    assert nft.balanceOf(accounts[2]) == 10000
+
+
+def test_zero_tokens_transfer(accounts, nft):
+    ranges = nft.rangesOf(accounts[2])
+    nft.transfer(accounts[2], 0, {"from": accounts[2]})
+
+    assert nft.rangesOf(accounts[2]) == ranges
+    assert nft.balanceOf(accounts[2]) == 10000
+
+
+def test_zero_tokens_transferFrom(accounts, nft):
+    ranges = nft.rangesOf(accounts[2])
     nft.transferFrom(accounts[2], accounts[1], 0, {"from": accounts[1]})
+
+    assert nft.rangesOf(accounts[2]) == ranges
+    assert nft.balanceOf(accounts[2]) == 10000
